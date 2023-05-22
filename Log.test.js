8d40764c5cc2aaeab91da8440d5ca8373625cc06
Madelane <madelanedaz@gmail.com>
2023-05-23 00:12:39 +0100

    add test for getDebit method

--- Log.test.js
+++ Log.test.js
@@ -17,6 +17,11 @@ describe("Log class", () => {
     expect(logInstance.getCredit()).toEqual(100.00);
   })
 
+  it("should return debit value", () => {
+    const logInstance = new Log(0, 100, mockDate);
+    expect(logInstance.getDebit()).toEqual(100.00);
+  })
+
   it("should format date to dd/mm/yyyy", () => {
     const logInstance = new Log(0, 0, mockDate);
     expect(logInstance.formatDate()).toBe('22/05/2023');
@@ -25,11 +30,6 @@ describe("Log class", () => {
   it("should format number with 2 decimal places", () => {
     const logInstance = new Log(1000, 0, mockDate);
     expect(logInstance.credit).toEqual(1000.00);
-  })
-
-  it("should format number with 2 decimal places", () => {
-    const logInstance = new Log(100.505, 0, mockDate);
-    expect(logInstance.credit).toEqual(100.50);
   })
 
 });
