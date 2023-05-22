e59a231e533c7aa2f3da494af477c4e7f341ea4e
Madelane <madelanedaz@gmail.com>
2023-05-22 14:31:59 +0100

    test drive formatDate() method that formats date to dd/mm/yyyy

--- Log.js
+++ Log.js
@@ -5,6 +5,15 @@ class Log {
     this.balance = balance,
     this.date = date
   }
+
+  formatDate() {
+    let formattedDate = this.date.toISOString().slice(0,10);
+    const day = formattedDate.slice(8,10);
+    const month = formattedDate.slice(5,7);
+    const year = formattedDate.slice(0,4);
+
+    return `${day}/${month}/${year}`;
+  }
 };
 
 module.exports = Log;
--- Log.test.js
+++ Log.test.js
@@ -1,11 +1,20 @@
 const Log = require("./Log");
 
 describe("Log class", () => {
-  it("should construct a log instance", () => {
-    const mockDate = new Date('Mon May 22 2023 13:48:20 GMT+0100 (British Summer Time)');
+  let mockDate;
+  beforeEach(() => {
+    mockDate = new Date('Mon May 22 2023 13:48:20 GMT+0100 (British Summer Time)');
     jest.spyOn(global, 'Date').mockImplementation(() => mockDate);
+  })
 
+  it("should construct a log instance", () => {
     const logInstance = new Log(0, 0, 0, mockDate);
     expect(logInstance).toBeDefined();
   })
+
+  it("should format date to dd/mm/yyyy", () => {
+    const logInstance = new Log(0, 0, 0, mockDate);
+    expect(logInstance.formatDate()).toEqual('22/05/2023');
+  })
+
 });
