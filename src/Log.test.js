2f9a8b988849b2b237a9b0a0090d792b714a4d69
Madelane <madelanedaz@gmail.com>
2023-05-23 10:31:08 +0100

    add test for formatNumber method in Log class

--- src/Log.test.js
+++ src/Log.test.js
@@ -29,51 +29,65 @@ describe("Log class", () => {
     expect(logInstance.formatDate()).toBe("22/05/2023");
   });
 
-  it("should format number with 2 decimal places", () => {
-    const logInstance = new Log(1000, 0, mockDate);
-    expect(logInstance.credit).toEqual("1000.00");
+  describe("formatNumber", () => {
+    it("should format credit with 2 decimal places when it is more than zero", () => {
+      const logInstance = new Log(1000, 0, mockDate);
+      expect(logInstance.getCredit()).toEqual("1000.00");
+    });
+
+    it("should format credit with empty string when it is zero", () => {
+      const logInstance = new Log(0, 0, mockDate);
+      expect(logInstance.getCredit()).toEqual("");
+    });
+
+    it("should format debit with 2 decimal places when it is more than zero", () => {
+      const logInstance = new Log(0, 100, mockDate);
+      expect(logInstance.getDebit()).toEqual("100.00");
+    });
   });
 
-  it("should format log with credit value", () => {
-    const logInstance = new Log(1000, 0, mockDate);
-    const mockBankAccount = {
-      balance: 0,
-      getBalance: function () {
-        return this.balance.toFixed(2);
-      },
-      deposit: function (logInstance) {
-        this.balance += parseFloat(logInstance.getCredit());
-      },
-    };
+  describe("formatLog method", () => {
+    it("should format log with credit value", () => {
+      const logInstance = new Log(1000, 0, mockDate);
+      const mockBankAccount = {
+        balance: 0,
+        getBalance: function () {
+          return this.balance.toFixed(2);
+        },
+        deposit: function (logInstance) {
+          this.balance += parseFloat(logInstance.getCredit());
+        },
+      };
 
-    mockBankAccount.deposit(logInstance);
+      mockBankAccount.deposit(logInstance);
 
-    expect(logInstance.formatLog(mockBankAccount.getBalance())).toEqual(
-      "22/05/2023 || 1000.00 || || 1000.00"
-    );
-  });
+      expect(logInstance.formatLog(mockBankAccount.getBalance())).toEqual(
+        "22/05/2023 || 1000.00 || || 1000.00"
+      );
+    });
 
-  it("should format log with debit value", () => {
-    const logInstance = new Log(2000, 0, mockDate);
-    const logInstance2 = new Log(0, 500, mockDate);
-    const mockBankAccount = {
-      balance: 0,
-      getBalance: function () {
-        return this.balance.toFixed(2);
-      },
-      deposit: function (logInstance) {
-        this.balance += parseFloat(logInstance.getCredit());
-      },
-      withdraw: function (logInstance) {
-        this.balance -= parseFloat(logInstance.getDebit());
-      },
-    };
+    it("should format log with debit value", () => {
+      const logInstance = new Log(2000, 0, mockDate);
+      const logInstance2 = new Log(0, 500, mockDate);
+      const mockBankAccount = {
+        balance: 0,
+        getBalance: function () {
+          return this.balance.toFixed(2);
+        },
+        deposit: function (logInstance) {
+          this.balance += parseFloat(logInstance.getCredit());
+        },
+        withdraw: function (logInstance) {
+          this.balance -= parseFloat(logInstance.getDebit());
+        },
+      };
 
-    mockBankAccount.deposit(logInstance);
-    mockBankAccount.withdraw(logInstance2);
+      mockBankAccount.deposit(logInstance);
+      mockBankAccount.withdraw(logInstance2);
 
-    expect(logInstance2.formatLog(mockBankAccount.getBalance())).toEqual(
-      "22/05/2023 || || 500.00 || 1500.00"
-    );
+      expect(logInstance2.formatLog(mockBankAccount.getBalance())).toEqual(
+        "22/05/2023 || || 500.00 || 1500.00"
+      );
+    });
   });
 });
